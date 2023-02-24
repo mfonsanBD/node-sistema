8d122ebf40a6601765916553f91a1166abc2049e
Mike Santos <iddeiasimplesmike@gmail.com>
2023-02-23 22:44:53 -0300

    chore: add scripts to package error 500 in english

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -3,6 +3,6 @@ import { ApiError } from '../helpers/api-errors'
 
 export const errorMiddleware = (error: Error & Partial<ApiError>, req: Request, res: Response, next: NextFunction) => {
   const statusCode = error.statusCode ?? 500
-  const message = error.statusCode ? error.message : 'Erro Interno do Servidor'
+  const message = error.statusCode ? error.message : 'Internal Server Error'
   return res.status(statusCode).json({ message })
 }
